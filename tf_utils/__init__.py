47c7283630db67121d3453437f251a5c2d47ef12
Misha Wagner <mishajw@gmail.com>
2017-10-26 10:29:17 +0100

    Add hyperparameter_optimizer

--- tf_utils/__init__.py
+++ tf_utils/__init__.py
@@ -2,5 +2,6 @@
 
 from tf_utils import data_holder
 from tf_utils import generic_runner
+from tf_utils import hyperparameter_optimizer
 from tf_utils import model_saver
 from tf_utils.tf_utils import *
---
+++ tf_utils/hyperparameter_optimizer.py
@@ -0,0 +1,132 @@
+from typing import TypeVar, Generic, List, Any, Callable, Optional
+import io
+import math
+import os
+import skopt
+import subprocess
+import sys
+
+ParameterType = TypeVar("ParameterType")
+
+
+class OptimizableParameter(Generic[ParameterType]):
+    def __init__(
+            self,
+            command_line_name: str,
+            short_name: Optional[str],
+            skopt_dimension: Any,
+            value_to_string: Callable[[ParameterType], str] = str):
+        self.command_line_name = command_line_name
+        self.short_name = short_name
+        self.skopt_dimension = skopt_dimension
+        self.value_to_string_fn = value_to_string
+
+        if self.short_name is None:
+            self.short_name = "".join(
+                [word[0] for word in self.command_line_name.replace("-", "").split("_") if word != ""])
+
+    def get_command_line_argument(self, value: ParameterType) -> List[str]:
+        return [self.command_line_name, self.value_to_string_fn(value)]
+
+    def get_run_tag(self, value: ParameterType) -> str:
+        assert self.short_name is not None
+        return f"{self.short_name}{self.value_to_string_fn(value)}"
+
+    @classmethod
+    def int_type(cls, command_line_name: str, short_name: Optional[str], min_value: int, max_value: int):
+        return OptimizableParameter(command_line_name, short_name, (min_value, max_value))
+
+    @classmethod
+    def float_type(cls, command_line_name: str, short_name: Optional[str], min_value: float, max_value: float):
+        return OptimizableParameter(command_line_name, short_name, (min_value, max_value))
+
+    @classmethod
+    def int_power_type(
+            cls,
+            command_line_name: str,
+            short_name: Optional[str],
+            min_value: int,
+            max_value: int,
+            base: int = 2):
+        min_pow = int(math.log(min_value, base))
+        max_pow = int(math.log(max_value, base))
+        possible_values_str = [str(int(pow(base, p))) for p in range(min_pow, max_pow + 1)]
+
+        return OptimizableParameter(command_line_name, short_name, possible_values_str)
+
+    @classmethod
+    def bool_type(cls, command_line_name: str, short_name: Optional[str]):
+        return OptimizableParameter(command_line_name, short_name, ["true", "false"])
+
+    @staticmethod
+    def bind_with_parameters(optimizable_parameters: List["OptimizableParameter"], parameters: List[Any]):
+        assert len(parameters) == len(optimizable_parameters)
+
+        command_line_arguments = [optimizable.get_command_line_argument(p)
+                                  for optimizable, p in zip(optimizable_parameters, parameters)]
+
+        # Flatten the results
+        return [part for cli in command_line_arguments for part in cli]
+
+    @staticmethod
+    def get_full_run_tag(optimizable_parameters: List["OptimizableParameter"], parameters: List[Any]) -> str:
+        assert len(parameters) == len(optimizable_parameters)
+
+        return "_".join([optimizable.get_run_tag(p) for optimizable, p in zip(optimizable_parameters, parameters)])
+
+
+def create_function(base_command: List[str], optimizable_parameters: List[OptimizableParameter], log_directory: str):
+    def run_with_parameters(parameters):
+        print("starting")
+        last_output = None
+
+        run_tag = OptimizableParameter.get_full_run_tag(optimizable_parameters, parameters)
+        with open(os.path.join(log_directory, run_tag), "w") as process_log_file:
+            print(subprocess.check_output(["pwd"]))
+            print(subprocess.check_output(["ls"]))
+
+            process = subprocess.Popen(
+                base_command +
+                OptimizableParameter.bind_with_parameters(optimizable_parameters, parameters) +
+                ["--run_tag", run_tag],
+                stdout=subprocess.PIPE)
+
+            # Read stdout from the process
+            # while True:
+            for process_output in io.TextIOWrapper(process.stdout):
+                # process_output = process.stdout.readline().decode()
+                #
+                # if process_output == "":
+                #     break
+
+                # Write the logs to a file
+                process_log_file.write(process_output)
+
+                # Store the last output for cost parsing
+                last_output = process_output
+
+        try:
+            return float(last_output)
+        except (ValueError, TypeError):
+            print(f"Couldn't parse {last_output} as float for cost", file=sys.stderr)
+            return None
+
+    return run_with_parameters
+
+
+def run(
+        base_command: List[str],
+        optimizable_parameters: List[OptimizableParameter],
+        log_directory: str,
+        num_concurrent: int):
+
+    if not os.path.isdir(log_directory):
+        os.mkdir(log_directory)
+
+    run_with_parameters = create_function(base_command, optimizable_parameters, log_directory)
+
+    skopt.gp_minimize(
+        run_with_parameters,
+        dimensions=[optimizable.skopt_dimension for optimizable in optimizable_parameters],
+        acq_optimizer="lbfgs",
+        n_jobs=num_concurrent)
